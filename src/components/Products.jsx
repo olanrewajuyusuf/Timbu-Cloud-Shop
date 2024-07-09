5e60219a8f706b8cab2ea53e65c07d7dfd3e465b
Wallewdev <wallewdev@gmail.com>
2024-07-09 22:18:12 +0100

    homepage

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,7 +4,7 @@ import { desktopProducts } from '../data/Desktop/Products'
 
 const Products = () => {
   return (
-    <div className='px-5 pt-20 md:pt-40 md:px-10'>
+    <div className='px-5 pt-20 md:pt-28 md:px-10'>
         <div className='text-[#333333] text-[20px] md:text-[36px] border-b-[1px] border-black font-bold mt-10'>
             <p>Bridal Headgears</p>
         </div>
